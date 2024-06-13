9fe8cf0dbd6c68860425f695faf2a1744d5383eb
Camou <167819732+xCamou@users.noreply.github.com>
2024-06-13 13:48:01 +0200

    Update server.lua
    QB Core added

--- server.lua
+++ server.lua
@@ -1,36 +1,68 @@
-ESX = exports['es_extended']:getSharedObject()
+local Framework = nil
+
+if Config.Framework == 'ESX' then
+    Framework = exports['es_extended']:getSharedObject()
+elseif Config.Framework == 'QBCore' then
+    Framework = exports['qb-core']:GetCoreObject()
+end
 
 for _, recipe in ipairs(Config.Recipes) do
-    ESX.RegisterUsableItem(recipe.usableItem, function(source)
-        print("Usable item used: " .. recipe.usableItem)
-        TriggerClientEvent('crafting:startCrafting', source, recipe.usableItem)
-    end)
+    if Config.Framework == 'ESX' then
+        ESX.RegisterUsableItem(recipe.usableItem, function(source)
+            TriggerClientEvent('crafting:startCrafting', source, recipe.usableItem)
+        end)
+    elseif Config.Framework == 'QBCore' then
+        QBCore.Functions.CreateUseableItem(recipe.usableItem, function(source)
+            TriggerClientEvent('crafting:startCrafting', source, recipe.usableItem)
+        end)
+    end
 end
 
 RegisterServerEvent('crafting:removeIngredients')
 AddEventHandler('crafting:removeIngredients', function(recipe)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    print("Removing ingredients for recipe result: " .. recipe.result.name)
-
-    local hasAllIngredients = true
-    for _, ingredient in ipairs(recipe.ingredients) do
-        local itemCount = xPlayer.getInventoryItem(ingredient.name).count
-        print("Checking ingredient: " .. ingredient.name .. ", needed: " .. ingredient.count .. ", player has: " .. itemCount)
-        if itemCount < ingredient.count then
-            hasAllIngredients = false
-            break
-        end
-    end
-
-    if hasAllIngredients then
+    local src = source
+    if Config.Framework == 'ESX' then
+        local xPlayer = ESX.GetPlayerFromId(src)
+        local hasAllIngredients = true
         for _, ingredient in ipairs(recipe.ingredients) do
-            xPlayer.removeInventoryItem(ingredient.name, ingredient.count)
+            local itemCount = xPlayer.getInventoryItem(ingredient.name).count
+            if itemCount < ingredient.count then
+                hasAllIngredients = false
+                break
+            end
         end
-        xPlayer.addInventoryItem(recipe.result.name, recipe.result.count)
-        TriggerClientEvent('esx:showNotification', source, 'Du hast ' .. recipe.result.count .. 'x ' .. ESX.GetItemLabel(recipe.result.name) .. ' hergestellt.')
-    else
-        TriggerClientEvent('esx:showNotification', source, 'Du hast nicht alle notwendigen Zutaten.')
+
+        if hasAllIngredients then
+            for _, ingredient in ipairs(recipe.ingredients) do
+                xPlayer.removeInventoryItem(ingredient.name, ingredient.count)
+            end
+            xPlayer.addInventoryItem(recipe.result.name, recipe.result.count)
+                --add your notification
+            TriggerClientEvent("SService:Client:MakeInfoNotify", src, 'success', '[Crafting]', 'Du hast ' .. recipe.result.count .. 'x ' .. ESX.GetItemLabel(recipe.result.name) .. ' hergestellt.', 5000)
+        else
+                --add your notification
+            TriggerClientEvent("SService:Client:MakeInfoNotify", src, 'error', '[Crafting]', 'Du hast nicht alle notwendigen Zutaten.', 5000)
+        end
+    elseif Config.Framework == 'QBCore' then
+        local Player = QBCore.Functions.GetPlayer(src)
+        local hasAllIngredients = true
+        for _, ingredient in ipairs(recipe.ingredients) do
+            local item = Player.Functions.GetItemByName(ingredient.name)
+            if not item or item.amount < ingredient.count then
+                hasAllIngredients = false
+                break
+            end
+        end
+
+        if hasAllIngredients then
+            for _, ingredient in ipairs(recipe.ingredients) do
+                Player.Functions.RemoveItem(ingredient.name, ingredient.count)
+            end
+            Player.Functions.AddItem(recipe.result.name, recipe.result.count)
+            TriggerClientEvent("QBCore:Notify", src, 'Du hast ' .. recipe.result.count .. 'x ' .. QBCore.Shared.Items[recipe.result.name].label .. ' hergestellt.', 'success')
+        else
+            TriggerClientEvent("QBCore:Notify", src, 'Du hast nicht alle notwendigen Zutaten.', 'error')
+        end
     end
 end)
-
 
