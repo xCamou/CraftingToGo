67b98c15abce1c3b8c3a811e7547ae4fd04a3f8c
Camou <167819732+xCamou@users.noreply.github.com>
2024-06-13 13:45:45 +0200

    Update client.lua
    QB Core added

--- client.lua
+++ client.lua
@@ -1,19 +1,32 @@
-ESX = exports['es_extended']:getSharedObject()
+local Framework = nil
+
+if Config.Framework == 'ESX' then
+    Framework = exports['es_extended']:getSharedObject()
+elseif Config.Framework == 'QBCore' then
+    Framework = exports['qb-core']:GetCoreObject()
+end
 
 RegisterNetEvent('crafting:startCrafting')
 AddEventHandler('crafting:startCrafting', function(usableItem)
     local playerPed = PlayerPedId()
-    print("Crafting started with item: " .. usableItem)
 
     for _, recipe in ipairs(Config.Recipes) do
         if recipe.usableItem == usableItem then
-            print("Found recipe for item: " .. usableItem)
-            ESX.Streaming.RequestAnimDict(recipe.animation.dict, function()
-                TaskPlayAnim(playerPed, recipe.animation.dict, recipe.animation.name, 8.0, -8.0, recipe.animation.time, 1, 0, false, false, false)
-                Citizen.Wait(recipe.animation.time)
-                ClearPedTasks(playerPed)
-                TriggerServerEvent('crafting:removeIngredients', recipe)
-            end)
+            if Config.Framework == 'ESX' then
+                ESX.Streaming.RequestAnimDict(recipe.animation.dict, function()
+                    TaskPlayAnim(playerPed, recipe.animation.dict, recipe.animation.name, 8.0, -8.0, recipe.animation.time, 1, 0, false, false, false)
+                    Citizen.Wait(recipe.animation.time)
+                    ClearPedTasks(playerPed)
+                    TriggerServerEvent('crafting:removeIngredients', recipe)
+                end)
+            elseif Config.Framework == 'QBCore' then
+                QBCore.Functions.RequestAnimDict(recipe.animation.dict, function()
+                    TaskPlayAnim(playerPed, recipe.animation.dict, recipe.animation.name, 8.0, -8.0, recipe.animation.time, 1, 0, false, false, false)
+                    Citizen.Wait(recipe.animation.time)
+                    ClearPedTasks(playerPed)
+                    TriggerServerEvent('crafting:removeIngredients', recipe)
+                end)
+            end
             break
         end
     end
