70ce2597da86e883ad1d68917fcd479bc3666e2b
xCamou <167819732+xCamou@users.noreply.github.com>
2024-12-07 17:14:23 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,16 +6,7 @@ description 'Crafting To Go'
 version '1.1'
 
 shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
 
-client_scripts {
-    'client.lua'
-}
-
-server_scripts {
-    'server.lua'
-}
-
-dependencies {
-    'es_extended', -- use ESX
-    --'qb-core' -- use QBCore
-}
+dependency 'es_extended'
